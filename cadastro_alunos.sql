8e68fd1d64a89da4a92403436e04d840a300ffd3
Stephany3pg <stephany3pg@gmail.com>
2016-12-13 13:55:47 -0200

    Create cadastro_alunos.sql

---
+++ cadastro_alunos.sql
@@ -0,0 +1,8 @@
+
+//teste
+insert into aluno(nome, documento, telefone, email)
+values ('Stephany Padilha Guimarães', '12', '34', 'stephany@aluno.com'),
+       ('João da Silva', '34','56', 'joaosilva@aluno.com'),
+       ('Maria Paula Pereira', '56', '78', 'mariapereira@aluno.com'),
+       ('Joana Pereira', '78', '910', 'joanapereira@aluno.com'),
+       ('Pedro Henrique Guimarães', '910', '1112', 'pedro@aluno.com');
